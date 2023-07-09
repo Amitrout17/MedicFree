b792bc7e0acdca8d78352d762350c00a1b332e43
AMIT ROUT <120554331+AmitRout1704@users.noreply.github.com>
2023-07-09 19:54:01 +0530

    app.js frontend updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -125,8 +125,8 @@ function App() {
               <Nav user={user} />
               <VerifyUser />
             </>
-          }
-        />
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
