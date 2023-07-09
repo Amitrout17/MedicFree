6b20b90ead1d3941000a109a150efa873d2abb7e
AMIT ROUT <120554331+AmitRout1704@users.noreply.github.com>
2023-07-10 03:05:14 +0530

    small changee

--- backend/routes/doctorEyeRouter.js
+++ backend/routes/doctorEyeRouter.js
@@ -7,7 +7,7 @@ const {
 
 const router = exprees.Router();
 
-router.route("/api/v1/doctoreye/add").post(getUserLogs);
+router.route("/api/v1/doctoreye/get/:id").post(getUserLogs);
 router.route("/api/v1/doctoreye/update/:id").post(updateUserLog);
 
 module.exports = router;
